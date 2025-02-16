50fbd1396c2ef1feb3786ba406b58ab8daaa57a5
alcatrazxlr8 <saransh1000@gmail.com>
2025-02-16 16:00:09 -0600

    changes to firebaseConfig.ts

--- src/firebaseConfig.ts
+++ src/firebaseConfig.ts
@@ -1,23 +1,3 @@
-// // Import the functions you need from the SDKs you need
-// import { initializeApp } from "firebase/app";
-// import { getAuth } from "firebase/auth";
-// // TODO: Add SDKs for Firebase products that you want to use
-// // https://firebase.google.com/docs/web/setup#available-libraries
-
-// // Your web app's Firebase configuration
-// const firebaseConfig = {
-//   apiKey: "AIzaSyB5oBkyh_oJDivIpbk--VJihY6JA7n5ZUQ",
-//   authDomain: "todo-list-128ca.firebaseapp.com",
-//   projectId: "todo-list-128ca",
-//   storageBucket: "todo-list-128ca.firebasestorage.app",
-//   messagingSenderId: "820855670824",
-//   appId: "1:820855670824:web:7ec88e6f5cc08aab38cd1d"
-// };
-
-// // Initialize Firebase
-// const app = initializeApp(firebaseConfig);
-// export const auth = getAuth(app);
-// export default app;
 
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
