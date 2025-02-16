e91be8eac29b058f111571624d2df068ce7a60bf
alcatrazxlr8 <saransh1000@gmail.com>
2025-02-16 17:38:49 -0600

    changed import paths to relative

--- src/main.tsx
+++ src/main.tsx
@@ -1,22 +1,3 @@
-// import React from "react";
-// import ReactDOM from "react-dom/client";
-// import { BrowserRouter, Routes, Route } from "react-router-dom";
-// import App from "./App";
-// import Login from "./pages/Login";
-// import { AuthProvider } from "./context/AuthContext";
-
-// ReactDOM.createRoot(document.getElementById("root")!).render(
-// 	<React.StrictMode>
-// 		<AuthProvider>
-// 			<BrowserRouter>
-// 				<Routes>
-// 					<Route path="/" element={<App />} />
-// 					<Route path="/login" element={<Login />} />
-// 				</Routes>
-// 			</BrowserRouter>
-// 		</AuthProvider>
-// 	</React.StrictMode>
-// );
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
